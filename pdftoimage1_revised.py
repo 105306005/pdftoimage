2f9a0b047e8b530d98c0d8ebea93c497dff39b67
maggiesunsun <31977424+105306005@users.noreply.github.com>
2022-03-08 00:47:04 -0500

    Add files via upload
    with yangyi's update

---
+++ pdftoimage1_revised.py
@@ -0,0 +1,91 @@
+# for pdf formate 1 - tag on the right of image + 3 images on one page
+# Reference 1 - How to Extract Images from PDF in Python : https://www.thepythoncode.com/article/extract-pdf-images-in-python 
+# Reference 2 - pic_getinfo: https://github.com/pymupdf/PyMuPDF/issues/1017
+# Reference 3 - textbox-extraction: https://github.com/pymupdf/PyMuPDF-Utilities/tree/master/textbox-extraction
+
+import fitz # PyMuPDF - pip3 install PyMuPDF Pillow
+import io
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# put your local path here
+path = "/Users/maggiesun/Documents/Testing/pdftoimage2-2/" 
+# put pdf file name
+pdf_file_name = 'test1.PDF' 
+# put words that can help us locate the correct page for image (ex. Title of that page)
+page_determinators = ["ADDITIONAL PHOTOGRAPH ADDENDUM", "Subject Photo Page"] 
+
+# -------------------------------------------------------------------------------
+
+def pic_getinfo(doc,page, item): 
+    """for getting image information"""  
+    xref, _, width, height, _, _, _, name , _ = item
+    rect = page.get_image_bbox(name) # bounding box of a image, left, top, width, height
+#     pix = fitz.Pixmap(page)
+    img = Image.open(io.BytesIO(doc.extractImage(xref)['image'])) # Image index number
+    return {"height":height,"width":width,"xref":xref, "name":name, "rect":rect,"img":img}
+
+def make_text(words):
+    """Return textstring output of get_text("words").
+    Word items are sorted for reading sequence left to right,
+    top to bottom.
+    """
+    line_dict = {}  # key: vertical coordinate, value: list of words
+    words.sort(key=lambda w: w[0])  # sort by horizontal coordinate
+    for w in words:  # fill the line dictionary
+        y1 = round(w[3], 1)  # bottom of a word: don't be too picky!
+        word = w[4]  # the text of the word
+        line = line_dict.get(y1, [])  # read current line content
+        line.append(word)  # append new word
+        line_dict[y1] = line  # write back to dict
+    lines = list(line_dict.items())
+    lines.sort()  # sort vertically
+    return "\n".join([" ".join(line[1]) for line in lines])
+
+#------------------------------------------------------------
+
+# open the file
+pdf_file = fitz.open(path+'/'+pdf_file_name)
+
+# iterate over PDF pages
+for page_index in range(len(pdf_file)):
+    # get the page itself
+    page = pdf_file[page_index]
+    image_list = page.getImageList(full=True)
+#     print(image_list)
+    # printing number of images found in this page - but may contain signiture or other things
+    if image_list:
+         print("[+] Found a total of "+ str(len(image_list))+" images in page" + str(page_index+1))
+    else:
+        print("[!] No images found on page", page_index)
+    text = page.get_text("text")
+#     print(text)
+    
+    if any(deter in text for deter in page_determinators):
+#         print(text)
+        for image_index, img in enumerate(page.get_images(), start=1):
+            # get the XREF of the image
+            xref = img[0]
+            # extract the image bytes
+            base_image = pdf_file.extract_image(xref)
+            # get image information
+            img_info = pic_getinfo(pdf_file,page, img)
+            # get image Rectangle
+            img_rect = img_info["rect"] #x0, y0, x1, y1 
+            #set word Rectangle that we want to detect -> note, Make decisions by guessing
+            word_rect = (img_rect[0], img_rect[1], img_rect[2]+1000, img_rect[3]+100)
+            # get image
+            image_bytes = base_image["image"]
+            # get the image extension - file type 
+            image_ext = base_image["ext"]
+            # load it to PIL
+            image = Image.open(io.BytesIO(image_bytes))
+            # use 96 * 96 to filter the blank image
+            if image.info['dpi'] != (96, 96):
+#                 image.show()
+                # save it to local disk
+                # page.get_textbox(word_rect) is Desired output - the word detect in word Rectangle
+                imgname = str(page.get_textbox(word_rect)) +'_' + pdf_file_name+'_' +str(page_index+1)+'_' +str(image_index)+'.'+ str(image_ext)
+                image.save(open(os.path.join(path, imgname), "wb"))
+
